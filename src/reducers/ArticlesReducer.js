1d0db4faff12f4882c678d3ac6880b7f9485e412
Zolfekar <zolfekaraskarieh@gmail.com>
2019-08-30 09:43:36 +0300

    fixing mobile bug

--- src/reducers/ArticlesReducer.js
+++ src/reducers/ArticlesReducer.js
@@ -9,10 +9,10 @@ const INIT_STATE = {
     articles: JSON.parse(localStorage.getItem('articles')) || []
 };
 
-export default (state = INIT_STATE, {type, payload = {} }) => {
-    const {success, articles} = payload;
+export default (state = INIT_STATE, {type, payload }) => {
     switch (type) {
         case ADD_ARTICLE: case DELETE_ARTICLE: case EDIT_ARTICLE:
+            const {success, articles} = payload;
             if(success) {
                 return { articles };
             }
